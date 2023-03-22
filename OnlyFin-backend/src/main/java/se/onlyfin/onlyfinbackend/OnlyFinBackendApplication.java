c9eb3e91d1c981d5f5e236226b9c40f6c0113ea8
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-03-22 14:30:35 +0100

    Created backend project files
    Created intellij project with Spring initializer for the backend part of the project

---
+++ OnlyFin-backend/src/main/java/se/onlyfin/onlyfinbackend/OnlyFinBackendApplication.java
@@ -0,0 +1,13 @@
+package se.onlyfin.onlyfinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyFinBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlyFinBackendApplication.class, args);
+    }
+
+}
