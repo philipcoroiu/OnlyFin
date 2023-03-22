31e6c7413f45ac2276c471823c02324f24685da3
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-03-22 15:04:45 +0100

    Added test method
    Added method that prins hello world on "/test"

--- OnlyFin-backend/src/main/java/se/onlyfin/onlyfinbackend/OnlyFinBackendApplication.java
+++ OnlyFin-backend/src/main/java/se/onlyfin/onlyfinbackend/OnlyFinBackendApplication.java
@@ -2,12 +2,20 @@ package se.onlyfin.onlyfinbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class OnlyFinBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlyFinBackendApplication.class, args);
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String testMethod() {
+        return "Hello world";
     }
 
 }
