586c5e305696f45851a2e7bf2aba4c1911bec231
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-03-22 15:22:39 +0100

    Recreated project structure
    Created front-end with react generator in intellij. Created backend with Spring initializer

---
+++ onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/OnlyfinBackendApplication.java
@@ -0,0 +1,13 @@
+package se.onlyfin.onlyfinbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlyfinBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlyfinBackendApplication.class, args);
+	}
+
+}
---
+++ onlyfin-backend/src/test/java/se/onlyfin/onlyfinbackend/OnlyfinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package se.onlyfin.onlyfinbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyfinBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
