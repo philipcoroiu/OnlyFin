3aab7a96f62a8b6c0f3fbb858eff930d1f59125b
FabianKjellberg <120188031+FabianKjellberg@users.noreply.github.com>
2023-03-22 15:02:49 +0100

    tog bort error kod

--- OnlyFin-backend/src/main/java/se/onlyfin/onlyfinbackend/OnlyFinBackendApplication.java
+++ OnlyFin-backend/src/main/java/se/onlyfin/onlyfinbackend/OnlyFinBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlyFinBackendApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(OnlyFinBackendApplication.class, args);
     }
 
