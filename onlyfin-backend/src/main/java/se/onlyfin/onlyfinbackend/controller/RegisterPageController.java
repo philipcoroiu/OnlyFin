8b5ba3ed7ea5db0e017082a0b38d0b9d135474d1
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-03-27 22:01:28 +0200

    Added demo register page

--- onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/controller/RegisterPageController.java
+++ onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/controller/RegisterPageController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexPageController {
+public class RegisterPageController {
 
-    @GetMapping("/")
-    public String indexPage() {
-        return "index.html";
+    @GetMapping("/register")
+    public String registerPage() {
+        return "register.html";
     }
 
 }
---
+++ onlyfin-backend/src/test/java/se/onlyfin/onlyfinbackend/HTMLPageTest.java
@@ -0,0 +1,19 @@
+package se.onlyfin.onlyfinbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class HTMLPageTest {
+
+    @Test
+    public void testIndexPage() {
+        //TODO add test
+    }
+
+    @Test
+    public void testRegisterPage() {
+        //TODO add test
+    }
+
+}
