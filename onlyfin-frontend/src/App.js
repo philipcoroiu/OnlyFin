39d239913ea1b08f46c301b93e37b861546445e0
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-04-05 11:12:24 +0200

    Front-end project setup
    Added dependencies: axios, router-dom.
    
    Added files and setup the router

--- onlyfin-frontend/src/App.js
+++ onlyfin-frontend/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes className="App">
+      <Route path="/" element={<Home />}></Route>
+      <Route path="Login">
+        <Route path="" element={<Login/>}></Route>
+        <Route path="Dashboard">
+          <Route path="" element={<Dashboard />}/>
+        </Route>
+      </Route>
+
+    </Routes>
   );
 }
 
