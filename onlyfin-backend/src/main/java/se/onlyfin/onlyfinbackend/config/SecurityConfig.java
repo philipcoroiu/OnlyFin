51c0224920693e02164fc150657a7cb9ee2e877e
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-04-13 16:34:05 +0200

    Update SecurityConfig.java

--- onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/config/SecurityConfig.java
+++ onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors();
+        http.cors().disable();
         http.csrf().disable()
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
