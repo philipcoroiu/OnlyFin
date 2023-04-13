bc3772c7c02f7c2d0eb20b9f271f1a361e76b8c8
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-04-13 16:01:41 +0200

    Update SecurityConfig.java

--- onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/config/SecurityConfig.java
+++ onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import se.onlyfin.onlyfinbackend.service.OnlyfinUserDetailsService;
 
 import java.util.Arrays;
@@ -64,7 +66,8 @@ public class SecurityConfig {
                         //uncomment the row below to enable user debug:
                         //.requestMatchers("/user-debug").permitAll()
                 )
-                .formLogin();
+                .formLogin()
+                .and().cors();
         return http.build();
     }
 
@@ -90,18 +93,5 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Requestor-Type"));
-        configuration.setExposedHeaders(Arrays.asList("X-Get-Header"));
-        configuration.setMaxAge(3600L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
 
 }
