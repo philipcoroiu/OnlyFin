bf732c65746255a5b18c5bd86a41ac1ab3e33510
Victor Pirojoc <120030846+Senpai-paj@users.noreply.github.com>
2023-04-14 17:44:55 +0200

    Design
    Added first test design to login page and studio

--- onlyfin-frontend/src/components/login/Login.js
+++ onlyfin-frontend/src/components/login/Login.js
@@ -1,23 +1,35 @@
 import React from "react"
 import LoginImg from "../../images/login-image.jpg"
-import { Link, Outlet } from "react-router-dom"
+import {Link, Outlet} from "react-router-dom"
+import backgroundImage from "../../images/background.jpg"
 
 export default function Login() {
-    return(
-        <div className="login--container">
+    return (
+        <div className="login--container"
+            /*style = {{
+                backgroundImage: `url(${backgroundImage})`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                height: '100vh',
+            }}*/
+        >
             <div className="login--credentials--container">
                 <div className="login--credentials">
                     <h1>Log in</h1>
                     <h3>Welcome to OnlyFin, please put your credentials below to start using the app</h3>
-                    <input type="text" placeholder="Email"></input>
-                    <input type="text" placeholder="Password"></input>
-                    <Link to="LoginTest">
-                        <button>Submit</button>
-                    </Link>
+                    <div className="login--inputs">
+                        <input type="text" placeholder="Email"></input>
+                        <input type="text" placeholder="Password"></input>
+                    </div>
+                    <div className="login--submit--holder">
+                        <Link to="LoginTest">
+                            <button className="login--submit">Submit</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
             <div>
-                <img src={LoginImg} className="login--img"/>
+                {/*<img src={LoginImg} className="login--img"/>*/}
             </div>
         </div>
     )
