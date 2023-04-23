948d7f56f67d6f60a4db6263605e94c85d93ef83
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-04-23 16:40:48 +0200

    first primitive feed functionality implemented

---
+++ onlyfin-frontend/src/components/feed/FeedModule.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+
+export default function FeedModule(props) {
+
+
+    return(
+        <div style={{ border: "1px solid #ccc", borderRadius: "4px", padding: "1rem", boxShadow: "0 2px 2px rgba(0,0,0,0.1)", maxWidth: "400px" }}>
+            <p>{props.posterOfContent}</p>
+            <p>{props.postDate}</p>
+            <p>{props.stock}</p>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={props.chart}/>
+        </div>
+
+    )
+
+}
---
+++ onlyfin-frontend/src/components/feed/FeedPage.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Feed from "./Feed"
+
+export default function FeedPage() {
+
+    return(
+        <Feed></Feed>
+    )
+}
