e7e6665d1a3d98af40f6fdd08e76006db45ab4a2
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-04-26 17:37:25 +0200

    Implemented working input field in dropdown

--- onlyfin-frontend/src/components/dashboard/CategoryDropdownMenu.js
+++ onlyfin-frontend/src/components/dashboard/CategoryDropdownMenu.js
@@ -1,27 +1,73 @@
-import React from "react"
+import React, {useEffect, useRef} from "react"
 
 export default function CategoryDropdownMenu() {
 
     const [showMenu, setShowMenu] = React.useState(false);
+    const [inputValue, setInputValue] = React.useState();
+    const dropdownRef = useRef(null);
+
+    /**
+     * The menu closes when clicked outside the dropdown menu
+     */
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setShowMenu(false);
+            }
+        }
+
+        window.addEventListener('click', handleClickOutside);
+        return () => window.removeEventListener('click', handleClickOutside);
+    }, [dropdownRef]);
 
     function handleToggleMenu() {
         setShowMenu(!showMenu);
     }
 
     function handleMenuItemClick(item) {
-
+        if(item === "Item 1") {
+            console.log(item)
+        } else if(item === "Item 2") {
+            console.log(item)
+        }
     }
 
+    function handleOnSubmit(event) {
+        setShowMenu(false);
+        setInputValue("");
+    }
+
+
     return (
-        <div>
+        <div ref={dropdownRef}>
             <button onClick={handleToggleMenu}>...</button>
             {
                 showMenu && (
-                        <ul>
-                            <li onClick={() => handleMenuItemClick('Item 1')}>Item 1</li>
-                            <li onClick={() => handleMenuItemClick('Item 2')}>Item 2</li>
-                            <li onClick={() => handleMenuItemClick('Item 3')}>Item 3</li>
-                        </ul>
+                    <div style={{
+
+                        // >>>> OBS – TA BORT DETTA VID DESIGN <<<<<
+
+                        border: "1px solid #ccc",
+                        borderRadius: "5px",
+                        boxShadow: "0 0 5px rgba(0,0,0,0.3)",
+                        backgroundColor: "#fff",
+                        padding: "10px"
+
+                        // >>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<
+                    }}>
+
+                        <button onClick={() => handleMenuItemClick('Item 1')}>Item 1</button>
+                        <button onClick={() => handleMenuItemClick('Item 2')}>Item 2</button>
+
+                        <form onSubmit={handleOnSubmit}>
+                            <input
+                                type="text"
+                                placeholder="Category Name"
+                                value={inputValue}
+                                onChange={(e) => setInputValue(e.target.value)}
+                            />
+                        </form>
+                    </div>
                 )
             }
         </div>
