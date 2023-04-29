5f1f717d94d4e85caaad5643b9368cce28b5b761
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-04-29 21:47:30 +0200

    Testing react-grid-layout

--- onlyfin-frontend/src/components/editableDashboard/DragAndDropList.js
+++ onlyfin-frontend/src/components/editableDashboard/DragAndDropList.js
@@ -1,87 +1,65 @@
-import React, {useEffect, useState} from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import React, { useEffect, useState } from "react";
+import { WidthProvider, Responsive } from "react-grid-layout";
 import axios from "axios";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+
+const ResponsiveGridLayout = WidthProvider(Responsive);
 
 const DraggableList = () => {
-
     const [dashboard, setDashboard] = useState(null);
-    const [stocks, setStocks] = useState([])
-    const [test, setTest] = useState([])
+    const [stocks, setStocks] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [userId, setUserId] = useState();
 
     useEffect(() => {
-
-        axios.get("http://localhost:8080/fetch-current-user-id", {withCredentials: true}).then((response) => {
-            setUserId(response.data)
-            console.log(response.data)
-            axios.get("http://localhost:8080/dashboard/" + response.data,
-                {withCredentials: true}).then((response) => {
-                setDashboard(response.data);
-                setStocks(response.data.stocks[0].categories[0].moduleEntities);
-
-                console.log("response.data.stocks[1].categories[0].moduleEntities: ", response.data.stocks[0].categories[0].moduleEntities);
-
-
-                setIsLoading(false);
+        axios
+            .get("http://localhost:8080/fetch-current-user-id", {
+                withCredentials: true,
+            })
+            .then((response) => {
+                setUserId(response.data);
+                axios
+                    .get("http://localhost:8080/dashboard/" + response.data, {
+                        withCredentials: true,
+                    })
+                    .then((response) => {
+                        setDashboard(response.data);
+                        setStocks(response.data.stocks[1].categories[0].moduleEntities);
+                        setIsLoading(false);
+                    });
             });
-        })
     }, []);
 
-
-
-    const onDragEnd = (result) => {
-        if (!result.destination) {
-            return;
-        }
-
-        const newItems = Array.from(stocks);
-        const [removed] = newItems.splice(result.source.index, 1);
-        newItems.splice(result.destination.index, 0, removed);
-
-        setStocks(newItems);
-    };
+    const layout = stocks.map((item, index) => ({
+        i: item.id.toString(),
+        x: index * 2 % 10,
+        y: Math.floor(index / 5) * 2,
+        w: 2,
+        h: 2,
+    }));
 
     return (
-        <DragDropContext onDragEnd={onDragEnd}>
-            <Droppable droppableId="droppable">
-                {(provided) => (
-                    <div {...provided.droppableProps} ref={provided.innerRef}>
-                        {stocks.map((item, index) => (
-                            <Draggable key={item.id} draggableId={item.id.toString()} index={index}>
-                                {(provided) => (
-                                    <div
-                                        ref={provided.innerRef}
-                                        {...provided.draggableProps}
-                                        {...provided.dragHandleProps}
-                                        style={{
-                                            border: '1px solid #ccc',
-                                            borderRadius: '4px',
-                                            padding: '16px',
-                                            margin: '8px',
-                                            width: '300px',
-                                            boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-                                        }}
-                                    >
-                                        <HighchartsReact
-                                            highcharts={Highcharts}
-                                            options={item.content}
-                                        />
-
-                                        <p>item id: {item.id}</p>
-                                    </div>
-                                )}
-                            </Draggable>
-                        ))}
-                        {provided.placeholder}
-                    </div>
-                )}
-            </Droppable>
-        </DragDropContext>
+        <ResponsiveGridLayout
+            className="layout"
+            layouts={{ lg: layout }}
+            breakpoints={{ lg: 1200 }}
+            cols={{ lg: 12 }}
+            rowHeight={150}
+            isResizable={true}
+        >
+            {stocks.map((item) => (
+                <div key={item.id} style={{ background: "#fff", padding: "8px" }}>
+                    <HighchartsReact
+                        highcharts={Highcharts}
+                        options={item.content}
+                    />
+                </div>
+            ))}
+        </ResponsiveGridLayout>
     );
-
 };
 
 export default DraggableList;
