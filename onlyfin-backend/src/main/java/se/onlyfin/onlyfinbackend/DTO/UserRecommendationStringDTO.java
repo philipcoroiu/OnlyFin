4ab7c0d076ef841701c95ee663f34a0f83983878
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-05-09 17:36:16 +0200

    Probably fixed duplicate user suggestion entries

--- onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/DTO/UserRecommendationStringDTO.java
+++ onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/DTO/UserRecommendationStringDTO.java
@@ -1,4 +1,18 @@
 package se.onlyfin.onlyfinbackend.DTO;
 
+import java.util.Objects;
+
 public record UserRecommendationStringDTO(String stock, ProfileDTO profileDTO) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserRecommendationStringDTO that = (UserRecommendationStringDTO) o;
+        return Objects.equals(profileDTO, that.profileDTO);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(profileDTO);
+    }
 }
