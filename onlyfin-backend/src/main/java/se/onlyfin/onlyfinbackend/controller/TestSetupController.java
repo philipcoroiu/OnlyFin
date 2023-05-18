bafa724dc5f850da3946969d1b2238e3ecb5673b
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-05-18 16:43:30 +0200

    Updated Test-endpoints
    Added backend test endpoint integration for tests that were implemented in the test document: search, login, register, profile, subscribe

--- onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/controller/TestSetupController.java
+++ onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/controller/TestSetupController.java
@@ -1,9 +1,20 @@
 package se.onlyfin.onlyfinbackend.controller;
 
+import jakarta.transaction.Transactional;
 import org.springframework.web.bind.annotation.*;
 import se.onlyfin.onlyfinbackend.DTO.UserDTO;
+import se.onlyfin.onlyfinbackend.model.AnalystReview;
 import se.onlyfin.onlyfinbackend.model.User;
+import se.onlyfin.onlyfinbackend.model.dashboard_entity.Category;
+import se.onlyfin.onlyfinbackend.model.dashboard_entity.Dashboard;
+import se.onlyfin.onlyfinbackend.model.dashboard_entity.Stock;
+import se.onlyfin.onlyfinbackend.model.dashboard_entity.StockRef;
+import se.onlyfin.onlyfinbackend.repository.AnalystReviewRepository;
+import se.onlyfin.onlyfinbackend.repository.CategoryRepository;
+import se.onlyfin.onlyfinbackend.repository.StockRepository;
 import se.onlyfin.onlyfinbackend.service.UserService;
+
+import java.security.Principal;
 
 /**
  * This class is responsible for setting up user accounts that are meant to be used in black-box tests of the system.
@@ -12,34 +23,63 @@ import se.onlyfin.onlyfinbackend.service.UserService;
 @RequestMapping("/tests")
 @CrossOrigin(origins = "localhost:3000", allowCredentials = "true")
 public class TestSetupController {
-    private static final String TEST_PRODUCER_USERNAME = "TEST_PRODUCER_DONT_USE";
-    private static final String TEST_PRODUCER_PASSWORD = "TEST_PRODUCER_DONT_USE";
-    private static final String TEST_PRODUCER_EMAIL = "TEST_PRODUCER_DONT_USE@example.org";
+    private static final String TEST_PRODUCER_USERNAME = "TEST_PROD";
+    private static final String TEST_PRODUCER_PASSWORD = "TEST_PROD";
+    private static final String TEST_PRODUCER_EMAIL = "TEST_PROD@example.org";
     private final UserDTO TEST_PRODUCER_DTO;
 
-    private static final String TEST_CONSUMER_USERNAME = "TEST_CONSUMER_DONT_USE";
-    private static final String TEST_CONSUMER_PASSWORD = "TEST_CONSUMER_DONT_USE";
-    private static final String TEST_CONSUMER_EMAIL = "TEST_CONSUMER_DONT_USE@example.org";
+    private static final String TEST_CONSUMER_USERNAME = "TEST_CON";
+    private static final String TEST_CONSUMER_PASSWORD = "TEST_CON";
+    private static final String TEST_CONSUMER_EMAIL = "TEST_CON@example.org";
     private final UserDTO TEST_CONSUMER_DTO;
 
     private final UserService userService;
     private final SubscriptionController subscriptionController;
     private final AnalystReviewController analystReviewController;
+    private final AnalystReviewRepository analystReviewRepository;
+    private final StudioController studioController;
+    private final StockReferenceController stockReferenceController;
+    private final StockRepository stockRepository;
+    private final CategoryRepository categoryRepository;
 
-    public TestSetupController(UserService userService, SubscriptionController subscriptionController, AnalystReviewController analystReviewController) {
+    public TestSetupController(UserService userService, SubscriptionController subscriptionController, AnalystReviewController analystReviewController, AnalystReviewRepository analystReviewRepository, StudioController studioController, StockReferenceController stockReferenceController, StockRepository stockRepository, CategoryRepository categoryRepository) {
         this.userService = userService;
         this.subscriptionController = subscriptionController;
         this.analystReviewController = analystReviewController;
+        this.analystReviewRepository = analystReviewRepository;
+        this.studioController = studioController;
+        this.stockReferenceController = stockReferenceController;
+        this.stockRepository = stockRepository;
+        this.categoryRepository = categoryRepository;
 
         TEST_PRODUCER_DTO = new UserDTO(TEST_PRODUCER_EMAIL, TEST_PRODUCER_USERNAME, TEST_PRODUCER_PASSWORD);
         TEST_CONSUMER_DTO = new UserDTO(TEST_CONSUMER_EMAIL, TEST_CONSUMER_USERNAME, TEST_CONSUMER_PASSWORD);
     }
 
     @GetMapping("/reset")
-    public void reset() {
+    public void resetAllTheThings() {
         resetConsumer();
         resetProducer();
-        setup();
+        setupAccounts();
+    }
+
+    @GetMapping("/registration1")
+    public void setUpRegistration1() {
+        User targetUser = userService.getUserOrNull("TEST_REG");
+        if (targetUser != null) {
+            userService.deleteUser(targetUser);
+        }
+    }
+
+    @GetMapping("/profile2")
+    public void setUpProfile2() {
+        User targetUser = userService.getUserOrException("TEST_PROD");
+
+        AnalystReview analystReview = new AnalystReview();
+        analystReview.setAuthorUsername("TEST_CON");
+        analystReview.setTargetUser(targetUser);
+        analystReview.setReviewText("test review");
+        analystReviewRepository.save(analystReview);
     }
 
     public void resetConsumer() {
@@ -60,7 +100,7 @@ public class TestSetupController {
         }
     }
 
-    public void setup() {
+    public void setupAccounts() {
         User testProducer = userService.registerUser(TEST_PRODUCER_DTO);
         User testConsumer = userService.registerUser(TEST_CONSUMER_DTO);
         userService.enableAnalyst(testProducer);
