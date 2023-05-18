191fba2f3a5b3ad011b1e9139d0ba7089f1fe48b
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-05-18 19:50:44 +0200

    Implemented new hero page

--- onlyfin-frontend/src/components/home/Home.js
+++ onlyfin-frontend/src/components/home/Home.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {Link, NavLink} from "react-router-dom";
-import mallDashboard from "../../assets/images/mallDashboard.png";
+import Dashboard from "../../assets/images/Dashboard.png";
 import studioPic from "../../assets/images/studio.png";
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
     return (
         <div className="welcome">
             <nav className="welcome--navbar">
-                <h2 className="welcome--navbar--title">OnlyFin</h2>
+                <h2 className="welcome--navbar--title">OnlyFin <span>beta</span></h2>
                 <ul className="navbar--text">
                     <li>
                         <NavLink to="Login">
@@ -18,11 +18,11 @@ export default function Home() {
                 </ul>
             </nav>
             <div className="welcome--hero">
-                <h1>Welcome to <span className="welcome--hero--fin">OnlyFin</span></h1>
-                <h2>Create your own analysis...</h2>
-                <img className="welcome--hero--image" src={studioPic}/>
+                <h1 className="welcome--hero--fin">The OnlyFans<br/>of investing</h1>
+                <p className="welcome--hero--p">Make money on your stock<br/>market analysis</p>
+                <img className="welcome--hero--image" src={Dashboard}/>
                 <h2>...and share it with people around the world on your dashboard</h2>
-                <img className="welcome--hero--image" src={mallDashboard}/>
+                <img className="welcome--hero--image"  src={studioPic}/>
                 <Link to='Register'>
                     <button className="welcome--hero--button">Get Started</button>
                 </Link>
