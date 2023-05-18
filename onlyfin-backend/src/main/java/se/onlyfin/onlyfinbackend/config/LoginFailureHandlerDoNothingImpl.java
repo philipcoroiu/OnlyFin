38e04e8c5f87247865d1c043bdd3c4b2ec0f2624
Johannes <98045373+Luxcorel@users.noreply.github.com>
2023-05-18 11:54:43 +0200

    Now using HTTP 401 when getting auth failure

--- onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/config/LoginFailureHandlerDoNothingImpl.java
+++ onlyfin-backend/src/main/java/se/onlyfin/onlyfinbackend/config/LoginFailureHandlerDoNothingImpl.java
@@ -4,6 +4,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import java.io.IOException;
 
@@ -11,9 +12,9 @@ import java.io.IOException;
  * This class is used to override the default behavior of Spring Security's login failure handler.
  * This is to disable the redirect to the default error page.
  */
-public class LoginFailureHandlerDoNothingImpl implements org.springframework.security.web.authentication.AuthenticationFailureHandler {
+public class LoginFailureHandlerDoNothingImpl implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
