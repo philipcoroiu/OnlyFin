171a25bad63213b7e240411bf55f5a20d711d71c
philipcoroiu <108957226+philipcoroiu@users.noreply.github.com>
2023-05-18 21:36:40 +0200

    fixed spacing at bottom of screen

--- onlyfin-frontend/src/components/home/Home.js
+++ onlyfin-frontend/src/components/home/Home.js
@@ -84,7 +84,7 @@ export default function Home() {
                     <div className="welcome-section-five-content-right">
                         <h2>Get ahead of<br/>the curve</h2>
                         <p>Start building your name and reputation today by trying our platform and sharing your valuable insights on social media.
-                            
+
                             Together, we're revolutionizing the way we understand and profit from the stock market.</p>
                     </div>
                 </div>
